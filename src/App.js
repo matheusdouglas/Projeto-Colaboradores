63952853922c1248a2c8b72b15b8b2e846e15d58
Matheus Douglas <matheusdouglas1001@gmail.com>
2022-10-26 14:43:25 -0300

    Card-Colaboradores

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './componentes/Banner';
+import Formulario from './formulario';
+import { useState } from 'react'
+import Time from './Time';
+import Rodape from './Rodape';
+
 
 function App() {
+
+  const times = [
+   {
+    nome: 'desenvolvedores',
+    corPrimaria: '#57C278',
+    corSecundaria: '#D9F7E9',
+  },
+  {
+    nome: 'Qualidade',
+    corPrimaria: '#E06B69',
+    corSecundaria: '#FDE7E8',
+  },
+  {
+    nome: 'Lideres',
+    corPrimaria: '#82CFFA',
+    corSecundaria: '#E8F8FF',
+  }
+  ]
+
+  const [colaboradores, setColaboradores] = useState([])
+ 
+   const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+       
+      
+      
+           {times.map(time => <Time 
+           key={time.nome} 
+           nome={time.nome} 
+           corPrimaria={time.corPrimaria}
+            corSecundaria={time.corSecundaria}
+            colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+            
+            
+
+            />)}
+     
+     <Rodape />
     </div>
   );
 }
---
+++ src/Rodape/index.js
@@ -0,0 +1,13 @@
+//src/componentes/Rodape/index.js
+
+import './rodape.css'
+
+const Rodape = () => {
+    return (<footer className="banner">
+    <img src="/imagens/Rodapé.png" alt="Banner da pagina"/>
+</footer>)
+}
+
+export default Rodape
+
+
